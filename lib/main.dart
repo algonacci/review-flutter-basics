e5cb0aee5da95ac5d5ba2c1b4b9b40ac9af1042d
Eric Julianto <86970816+algonacci@users.noreply.github.com>
2022-11-27 17:02:45 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -22,67 +22,7 @@ class MyApp extends StatelessWidget {
             title: const Text('First Project'),
             backgroundColor: Colors.deepPurple,
           ),
-          body: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'Hello World!',
-                  style: TextStyle(
-                    fontSize: 28,
-                    color: Colors.pink,
-                  ),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    print('PRESSED');
-                  },
-                  child: Text(
-                    'Press Me!',
-                  ),
-                  style: ElevatedButton.styleFrom(
-                    padding: EdgeInsets.symmetric(
-                      horizontal: 50,
-                      vertical: 20,
-                    ),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(
-                        20,
-                      ),
-                    ),
-                  ),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                Container(
-                  alignment: Alignment.center,
-                  margin: EdgeInsets.all(25),
-                  padding: EdgeInsets.all(25),
-                  // color: Colors.yellow,
-                  child: Column(
-                    children: [
-                      Text(
-                        'This is the container',
-                        textAlign: TextAlign.center,
-                      ),
-                    ],
-                  ),
-                  decoration: BoxDecoration(
-                    color: Colors.amber,
-                    borderRadius: BorderRadius.circular(
-                      20,
-                    ),
-                  ),
-                  width: MediaQuery.of(context).size.width * 0.80,
-                  height: MediaQuery.of(context).size.height * 0.50,
-                ),
-              ],
-            ),
-          ),
+          body: containerLesson(),
           floatingActionButton: FloatingActionButton(
             onPressed: () {
               debugPrint('Button Clicked');
@@ -94,6 +34,139 @@ class MyApp extends StatelessWidget {
           ),
         );
       }),
+    );
+  }
+
+  Widget containerLesson() {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            'Hello World!',
+            style: TextStyle(
+              fontSize: 28,
+              color: Colors.pink,
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          ElevatedButton(
+            onPressed: () {
+              print('PRESSED');
+            },
+            child: const Text(
+              'Press Me!',
+            ),
+            style: ElevatedButton.styleFrom(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 50,
+                vertical: 20,
+              ),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(
+                  20,
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: [
+                logoContainer(),
+                logoContainer(),
+              ],
+            ),
+          ),
+          Row(
+            children: expandedContainer,
+          )
+        ],
+      ),
+    );
+  }
+
+  List<Widget> get expandedContainer {
+    return [
+      Expanded(
+        child: Container(
+          color: Colors.pink.shade100,
+          width: 200,
+          height: 200,
+        ),
+      ),
+      Expanded(
+        flex: 3,
+        child: Container(
+          color: Colors.red,
+          width: 300,
+          height: 200,
+        ),
+      ),
+      Expanded(
+        child: Container(
+          color: Colors.blue,
+          width: 300,
+          height: 200,
+        ),
+      ),
+      Flexible(
+        child: Container(
+          color: Colors.yellow,
+          width: 300,
+          height: 200,
+        ),
+      ),
+    ];
+  }
+
+  Widget logoContainer() {
+    return Container(
+      alignment: Alignment.center,
+      margin: const EdgeInsets.all(25),
+      padding: const EdgeInsets.all(25),
+      // color: Colors.yellow,
+      child: const FlutterLogo(
+        style: FlutterLogoStyle.horizontal,
+        size: 120,
+      ),
+      decoration: BoxDecoration(
+        color: Colors.amber,
+        border: Border.all(
+          width: 5,
+          color: Colors.purple,
+        ),
+        shape: BoxShape.rectangle,
+        borderRadius: const BorderRadius.only(
+          topLeft: Radius.circular(20),
+          bottomRight: Radius.circular(20),
+        ),
+        boxShadow: const [
+          BoxShadow(
+            color: Colors.pink,
+            offset: Offset(
+              10,
+              10,
+            ),
+            blurRadius: 20,
+          ),
+          BoxShadow(
+            color: Colors.purple,
+            offset: Offset(
+              -10,
+              -10,
+            ),
+            blurRadius: 20,
+          ),
+        ],
+      ),
+      // width: MediaQuery.of(context).size.width * 0.80,
+      // height: MediaQuery.of(context).size.height * 0.50,
     );
   }
 }
